f76e58b3af0a7f24dec78d5f0162298e6bb30876
Francois Reynaud <francois.reynaud@gmail.com>
2013-02-04 23:47:02 +0000

    all working.

--- src/main/java/org/uiautomation/iosdriver/WebInspector.java
+++ src/main/java/org/uiautomation/iosdriver/WebInspector.java
@@ -26,8 +26,28 @@ public class WebInspector {
   public static void main(String[] args) {
     WebInspector inspector = new WebInspector();
     inspector.start();
-    inspector.sendMessage("my xml");
+    inspector.sendMessage("<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +"\n"+
+                                  "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">" +"\n"+
+                                  "<plist version=\"1.0\">" +"\n"+
+                                  " <dict>" +"\n"+
+                                  " <key>__argument</key>" +"\n"+
+                                  " <dict>" +"\n"+
+                                  " <key>WIRConnectionIdentifierKey</key>" +"\n"+
+                                  " <string>9128c1d9-069d-4751-b45b-bbcb6e7e8591</string>" +"\n"+
+                                  " </dict>" +"\n"+
+                                  " <key>__selector</key>" +"\n"+
+                                  " <string>_rpc_reportIdentifier:</string>" +"\n"+
+                                  " </dict>" +"\n"+
+                                  "</plist>" +"\n");
     System.out.println(inspector.receiveMessage());
+    System.out.println(inspector.receiveMessage());
+    System.out.println(inspector.receiveMessage());
+    System.out.println(inspector.receiveMessage());
+    System.out.println(inspector.receiveMessage());
+    System.out.println(inspector.receiveMessage());
+    System.out.println(inspector.receiveMessage());
+
+
     inspector.stop();
   }
 }
--- src/main/java/org_uiautomation_iosdriver_WebInspector.c
+++ src/main/java/org_uiautomation_iosdriver_WebInspector.c
@@ -1,14 +1,26 @@
 #include <jni.h>
 #include <stdio.h>
+#include <string.h>
 #include "org_uiautomation_iosdriver_WebInspector.h"
 #include <libimobiledevice/libimobiledevice.h>
 #include <libimobiledevice/webinspector.h>
 
-static webinspector_client_t client = 0;
-
+static webinspector_client_t client = NULL;
+static idevice_t device = NULL;
 
 JNIEXPORT void JNICALL Java_org_uiautomation_iosdriver_WebInspector_start(JNIEnv * env, jobject thiz){
-	printf("Start!\n");
+	if (IDEVICE_E_SUCCESS != idevice_new(&device, NULL)) {
+        printf("No device found, is it plugged in?\n");
+        return;
+    }
+
+    webinspector_error_t error = webinspector_start_service(device, &client);
+    if (error != WEBINSPECTOR_E_SUCCESS) {
+        printf("Could not connect to the webinspector! Error: %i\n", error);
+        return;
+    }
+
+	printf("Started!\n");
     return;	
   }
 
@@ -16,22 +28,32 @@ JNIEXPORT void JNICALL Java_org_uiautomation_iosdriver_WebInspector_start(JNIEnv
 JNIEXPORT void JNICALL Java_org_uiautomation_iosdriver_WebInspector_stop(JNIEnv * env, jobject thiz){
   	printf("Stop!\n");
   	webinspector_client_free(client);
+  	idevice_free(device);
     return;	
   }
 
 JNIEXPORT jstring JNICALL Java_org_uiautomation_iosdriver_WebInspector_receiveMessage(JNIEnv* env,   jobject thiz){
-	jstring retval = (*env)->NewStringUTF(env, "test res");
 
+    char * buf = NULL;
+    uint32_t length = 0;
+    webinspector_error_t error = webinspector_receive_xml_plist(client, &buf, &length);
+    if (error != WEBINSPECTOR_E_SUCCESS || !buf || length == 0) {
+        printf("> Error reading: %d\n", error);
+    } else {
+        printf("> %d bytes\n%s===== end of message =====\n\n", length, buf);
+    }
+    jstring retval = (*env)->NewStringUTF(env, buf);
+    free(buf);
 
   	return retval;
   }
 
 JNIEXPORT void JNICALL Java_org_uiautomation_iosdriver_WebInspector_sendMessage(JNIEnv * env, jobject thiz, jstring command){
-	char buf[50];
-	const char *str = (*env)->GetStringUTFChars(env,command,0);
-
-  	printf("got message : %s\n",str);
-    return;	
+	const char * str = (*env)->GetStringUTFChars(env,command,0);
+    webinspector_send_xml_plist(client, str, strlen(str));
+    printf("Sent message : %s\n",str);
+    (*env)->ReleaseStringUTFChars(env,command,str);
+  	return;
   }
 
 
